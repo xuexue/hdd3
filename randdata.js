88a5082bd5aaad62d7a1d1c087f06740d6d096ea
Lisa Zhang <lz.weiqi@gmail.com>
2011-12-19 16:37:15 -0500

    make random data bigger
    * in terms of # dimensions
    * and in terms of # data points

--- randdata.js
+++ randdata.js
@@ -6,9 +6,11 @@ function randpoint() {
     z:Math.random()*3,
     t:Math.random()*7,
     a:Math.random()*4,
+    b:Math.random()*4,
+    c:Math.random()*4,
   }
 }
 var random = {
-  'traits': ['x', 'y', 'z', 't', 'a'],
-  'values' : d3.range(150).map(randpoint)
+  'traits': ['x', 'y', 'z', 't', 'a', 'b', 'c'],
+  'values' : d3.range(500).map(randpoint)
 };
